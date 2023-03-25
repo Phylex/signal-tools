b56f346a27bacddd1ae2a3f7e3178003d60f95de
Alexande Becker <a.becker@cern.ch>
2023-03-25 11:20:19 +0100

    updated commands and started to work on a common textual data format

--- signal_tools/cli.py
+++ signal_tools/cli.py
@@ -1,88 +1,182 @@
 from itertools import tee
 from pathlib import Path
-import pandas as pd
+import sys
+import csv
+import re
 import numpy as np
 import click
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from signal_tools.filter import trapezoid_filter
 
-from signal_tools.trapezfilter import trapezoid_filter
 
-
-@click.group()
-@click.argument("input", type=click.Path(dir_okay=False, exists=True))
-@click.option("-s", "--signal-column",
-              type=click.IntRange(min=0, max_open=True),
-              help="column of the data that should be used as the signal")
-@click.option("-f", "--filetype", type=click.Choice(["csv", "h5", "stream"]),
-              default="csv",
-              help="Type of file to be read in")
-@click.option("-d", "--delimiter", type=str, default=",",
-              help="item delimiter, only used with csv format")
-@click.option("-x", "--x-column", type=click.IntRange(min=0, max_open=True),
-              default=None,
-              help="Column in the Data used for the x-axis")
+@click.group("signal-io")
+@click.argument("file-path", type=click.Path(dir_okay=False))
+@click.argument("direction", type=click.Choice(["in", "out", "append"], case_sensitive=False))
+@click.argument("encoding", type=click.Choice(["bin", "utf-8"], case_sensitive=False))
 @click.pass_context
-def file_cli(ctx, input: click.Path,
-             signal_column: int, filetype: str,
-             delimiter: str,
-             x_column: int) -> None:
+def file_io(ctx, file_path: click.Path, direction: str, encoding: str) -> None:
+    """
+    Read from and write to files
+
+    FILE-PATH determins the file that is to be read from.
+    DIRECTION determins if data should be read from or written to the file and
+    ENCODING specifies if the file should be read as binary or utf-8 encoded file
+    """
     ctx.obj = {}
-    input_path = Path(str(input))
-    match filetype:
-        case "csv":
-            if input_path.suffix != "." + filetype:
-                click.echo(
-                    "Warning, file ending does not match the specified ending")
-            input_data = open(input_path, 'r')
-            i_data: pd.DataFrame = pd.read_csv(input_data, delimiter=delimiter)
-            signal_data: np.ndarray = i_data.iloc[:, signal_column].to_numpy()
-            signal_name: str = i_data.iloc[:, signal_column].name
-            if x_column is not None:
-                x_col = i_data.iloc[:, x_column].to_numpy()
-                x_name = i_data.iloc[:, x_column].name
-                ctx.obj["x"] = (x_name, x_col)
+    io_file = Path(str(file_path))
+    if (direction == 'in' or direction == "append") and not io_file.exists():
+        click.echo("File does not exist")
+        sys.exit(1)
+    ctx.obj = {}
+    file_mode = ""
+    match encoding:
+        case "bin":
+            file_mode += "b"
         case _:
-            click.echo("Filetype not implemented")
-            return
-    ctx.obj["y"] = (signal_name, signal_data)
-
-
-@click.group()
-@click.option("-s", "--signal-column", type=click.IntRange(0, max_open=True),
-              default=1)
-@click.option("-x", "--x-column", type=click.IntRange(0, max_open=True),
-              default=0)
-@click.option("-d", "--delimiter", type=click.Choice([",", ";", ":"]),
-              default=",")
-@click.pass_context
-def stream_cli(ctx: click.Context, signal_column: int, x_column: int,
-               delimiter: str):
-    """
-    Read in data from stdin and pass on to filter
-    """
-    in_stream = click.get_text_stream('stdin')
-    in_0, in_1 = tee(in_stream, 2)
-    x_data = map(lambda line: float(line.split(delimiter)[x_column].strip()), in_0)
-    signal = map(lambda line: float(line.split(delimiter)[signal_column].strip()), in_1)
-    ctx.obj = {"x": ("Samples", x_data), "y": ("Signal", signal)}
+            pass
+    match direction:
+        case "in":
+            file_mode += "r"
+            in_file = open(io_file, file_mode)
+            out_file = click.get_text_stream('stdout')
+        case "out":
+            file_mode = "w+"
+            out_file = open(io_file, file_mode)
+            in_file = click.get_text_stream('stdin')
+        case _:
+            click.echo("Invalid application state")
+            sys.exit(2)
+    ctx.obj = {'in': in_file,
+               'out': out_file}
 
 
 @click.command()
+@click.argument("delimiter", type=str)
+@click.option("-s", "--signal-column", type=int, multiple=True, required=True,
+              help="index of the column that should be read in")
 @click.pass_context
-def to_stream(ctx: click.Context) -> None:
+def read_csv(ctx, delimiter, signal_column):
     """
-    convert the file read in into a stream
+    read in a file of CSV format.
     """
-    output = click.get_text_stream("stdout")
-    _, sig_data = ctx.obj["y"]
-    try:
-        _, x_data = ctx.obj["x"]
-        for x_d, sig_d in zip(x_data, sig_data):
-            output.write(f"{x_d}, {sig_d}\n")
-    except KeyError:
-        for sig_d in sig_data:
-            output.write(f"{sig_d}\n")
+    colname_regex = re.compile(
+        '^(?P<colname>\w+([\t| ]+\w+)*)[\t| ]?(:(?P<type>[int|float|str][arr]?):)?\[(?P<prefix>[k|m|u|n|p|f|M|G|T|E|ki|Mi|Gi|Ti])?(?P<unit>[m|V|A|g|K|cd|s|N])\]$')
+    csvr = csv.reader(
+        ctx.obj['in'], delimiter=delimiter, skipinitialspace=True)
+    out = ctx.obj['out']
+    col_names = next(csvr)
+    column_descriptions = []
+    for i, name in enumerate(col_names):
+        cmatch = colname_regex.match(name)
+        if cmatch is None:
+            click.echo(f"'{name}'has an invalid format")
+            sys.exit(3)
+        column_descriptions.append({'name': cmatch.group('colname'),
+                                    'prefix': cmatch.group('prefix'),
+                                    'unit': cmatch.group('unit'),
+                                    'type': cmatch.group('type')
+                                    })
+    if len(col_names) < max(signal_column):
+        click.echo("Column requested that does not exist")
+        sys.exit(4)
+    requested_cols_desc = [column_descriptions[i] for i in signal_column]
+
+    # write the requested_cols to the data stream
+    for col in requested_cols_desc:
+        out.write(f"{col['name']}:")
+        if col['type'] is not None:
+            out.write(f"{col['type']}:")
+        if col['prefix'] is not None:
+            out.write(f"{col['prefix']}")
+        if col['unit'] is not None:
+            out.write(f"{col['unit']}")
+        out.write(";")
+    out.write('\n')
+
+    # now write the stream info to the
+    colcount = len(signal_column) - 1
+    error = False
+    for line in csvr:
+        req_data = map(lambda elem: elem[1], filter(
+            lambda elem: elem[0] in signal_column, enumerate(line)))
+        for i, (elem, descr) in enumerate(zip(req_data, column_descriptions)):
+            match descr['type']:
+                case None:
+                    out.write(elem + ";")
+                case 'str':
+                    out.write(elem + ";")
+                case 'int':
+                    try:
+                        pelem = int(elem)
+                        out.write(repr(pelem))
+                    except ValueError:
+                        out.write("0")
+                        error = True
+                        click.echo(
+                            f"Error parsing integer on line {csvr.line_num}, column {signal_column[i]}")
+                case 'float':
+                    try:
+                        pelem = float(elem)
+                        out.write(repr(pelem))
+                    except ValueError:
+                        out.write("0.0")
+                        error = True
+                        click.echo(
+                            f"Error parsing float on line {csvr.line_num}, column {signal_column[i]}")
+                case _:
+                    click.echo("Parser Failiure. Stopping")
+                    sys.exit(4)
+            if i < colcount:
+                out.write(";")
+        if error:
+            sys.exit(5)
+        out.write("\n")
     return
+
+
+@click.group()
+@click.pass_context
+def stream_cli(ctx: click.Context):
+    """
+    Handle all the parsing and pass the data to the subcommands
+    """
+    in_stream = click.get_text_stream('stdin')
+    metadata_line = in_stream.readline()
+    stream_descriptors = metadata_line.split(";")
+    descr_regex = re.compile(
+        r'(?P<name>\w+([\t| ]+\w+)*)(:(?P<type>[int|float|str](arr)?))?:((?P<prefix>[k|m|u|n|p|f|M|G|T|E|ki|Mi|Gi|Ti])?(?P<unit>[m|V|A|g|K|cd|s|N]))?')
+    meta_data = []
+    for sdescr in stream_descriptors:
+        m = descr_regex.match(sdescr)
+        if m is None:
+            click.echo("Malformed metadata line at beginning of stream")
+            sys.exit(4)
+        meta_data.append({'name': m.group('name'),
+                          'prefix': m.group('prefix'),
+                          'unit': m.group('unit'),
+                          'type': m.group('type')}
+                         )
+    line_regex = re.compile(
+        r'\[?([+|-]?[\d+|\d+\.\d+])[\],]?')
+
+    def parsed_stream():
+        for line in in_stream:
+            columns = [re.findall(line_regex, elem)
+                       for elem in line.split(';')]
+            parsed_cols = []
+            for elem, mdata in zip(columns, meta_data):
+                match mdata['type']:
+                    case 'int':
+                        parsed_cols.append(list(map(int, elem))[0])
+                    case 'float':
+                        parsed_cols.append(list(map(float, elem))[0])
+                    case 'intarr':
+                        parsed_cols.append(list(map(int, elem)))
+                    case 'floatarr':
+                        parsed_cols.append(list(map(float, elem)))
+            yield parsed_cols
+
+    ctx.obj = {'data': parsed_stream, 'metadata': meta_data}
 
 
 @click.command()
@@ -156,6 +250,7 @@ def apply_trapezoidal_filter(ctx: click.Context, k: int,
         out.write(f"{x_el},{sig_el}\n")
     out.close()
 
+
 @click.command()
 @click.pass_context
 def plot(ctx: click.Context):
@@ -169,7 +264,5 @@ stream_cli.add_command(apply_trapezoidal_filter)
 stream_cli.add_command(digitize)
 stream_cli.add_command(plot)
 
-file_cli.add_command(to_stream)
-file_cli.add_command(print_input)
-file_cli.add_command(digitize)
-file_cli.add_command(apply_trapezoidal_filter)
+file_io.add_command(print_input)
+file_io.add_command(read_csv)
